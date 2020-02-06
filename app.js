7ec74efa98af27e067edf8b8340f7a7e35e03395
James S Abreu <jeandsmith@protonmail.com>
2020-02-06 00:15:58 -0500

    Windows dev

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ let app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'); 
 
 app.get('/', (req, res) => {
-    res.render('main');
+    res.render('main'); 
 });
 
 app.listen(5000, () => {
