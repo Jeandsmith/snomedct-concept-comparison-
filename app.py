80f1def3ac3f8c611b5fff9babe956e358111baa
James <jeandsmith@protonmail.com>
2020-03-13 16:59:15 -0400

    Added Flask Configuration
    Now the server will load wv2 models at start up

--- app.py
+++ app.py
@@ -3,28 +3,27 @@ import itertools as it
 import db_requests as dbreq
 from time import sleep
 from flask_script import Server, Manager
-# from gensim import Ke
-# import threading
-
-def load_models():
-    model = kv.load_word2vec_format(
-        '../wikipedia-pubmed-and-PMC-w2v.bin', binary=True)
-    print(len(model.wv.vocab))
+import gens
 
 # Custom server to load data at server startup
 class MyServer(Server):
     def __call__(self, app, *args, **kargs):
-        load_models()
+
+        # Load the w2v model
+        gens.load_models()
+
         return Server.__call__(self, app, *args, **kargs)
 
 app = Flask(__name__, static_folder='public/')
+manager = Manager(app)
+manager.add_command('runserver', MyServer)
 
 # Handle home page
 @app.route('/')
 def home():
     return render_template('main.html')
 
-
+# Handles user requests 
 @app.route('/term-search', methods=["GET", "POST"])
 def term_search():
     # Handle post request
@@ -35,3 +34,5 @@ def term_search():
 
 # cursor.close()
 # connection.close()
+if __name__ == "__main__":
+    manager.run()
